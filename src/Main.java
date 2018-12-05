93b2d15247cc8c708ddfaba5594c8f200af087a1
tancfire <tancfire@laposte.net>
2018-12-05 21:18:58 +0100

    Add Speech

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,54 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import java.util.Locale;
+import javax.speech.AudioException;
+import javax.speech.Central;
+import javax.speech.EngineException;
+import javax.speech.synthesis.Synthesizer;
+import javax.speech.synthesis.SynthesizerModeDesc;
 
 public class Main {
 
+    public static void main(String[] args) throws IOException, InterruptedException, EngineException, AudioException {
+
+        // ---- Initialiaze the voice
+
+            // set property as Kevin Dictionary
+            System.setProperty("freetts.voices",
+                    "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+
+            // Register Engine
+            Central.registerEngineCentral
+                    ("com.sun.speech.freetts.jsapi.FreeTTSEngineCentral");
+
+            // Create a Synthesizer
+            Synthesizer synthesizer =
+                    Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));
+
+            // Allocate synthesizer
+            synthesizer.allocate();
+
+            // Resume Synthesizer
+            synthesizer.resume();
+
+            // speaks the given text until queue is empty.
+            synthesizer.speakPlainText("test", null);
+            synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
 
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        // write your code here
 
+         // ---- Initialize selenium (to control the browser)
 
+        //Initialize Chrome
         DesiredCapabilities capability = DesiredCapabilities.chrome();
 
         capability.setAcceptInsecureCerts(true);
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\tgeant\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
 
         ChromeOptions co = new ChromeOptions();
         WebDriver driver = new ChromeDriver(co);
@@ -45,23 +80,42 @@ public class Main {
         driver.switchTo().window(driver.getWindowHandles().toArray()[0].toString());
         searchbar.sendKeys("Hello."+ Keys.ENTER);
 
+        synthesizer.speakPlainText("Hello.", null);
+        synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
+
         for(int i=0; i< 100; i++) {
-            Thread.sleep(5000);
+
+
+            WebDriverWait wait = new WebDriverWait(driver, 30);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("snipTextIcon")));
+
             String string_line1 = line1.getText();
             System.out.println("bot 1: "+string_line1);
+
+            synthesizer.speakPlainText(string_line1, null);
+            synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
 
             driver.switchTo().window(driver.getWindowHandles().toArray()[1].toString());
             searchbar2.sendKeys(string_line1 + Keys.ENTER);
 
-            Thread.sleep(5000);
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("snipTextIcon")));
+
+
             String string_line2 = line12.getText();
             System.out.println("bot 2: "+string_line2);
+
+            synthesizer.speakPlainText(string_line2, null);
+            synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY);
 
             driver.switchTo().window(driver.getWindowHandles().toArray()[0].toString());
             searchbar.sendKeys(string_line2 +Keys.ENTER);
 
         }
 
+
+        // Deallocate the Synthesizer.
+        synthesizer.deallocate();
        // driver.close();
     }
 }
